31589ab4c903ec6f507d1deaafd95c7bede29138
munyaneza-w <w.munyaneza@alustudent.com>
2023-12-16 12:54:39 -0800

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,14 @@
-# Puppet manifest to fix a bug in wp-setings.php
+# increase ULIMTI
 
-exec { 'fix the php extension issue':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# declare nginx service and make sure it is running
+service { 'nginx':
+  ensure => running,
+}
+
+exec { 'increase ULIMIT':
+  # replace with new ULIMIT value
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/g" /etc/default/nginx',
+  provider => 'shell',
+  # tells nginx to restart
+  notify   => Service['nginx']
 }
